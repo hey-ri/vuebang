a7cd802b8ee458e5920a4dbeaa1dee5ea5a836fd
hey-ri <kwonhr01@naver.com>
2023-03-06 22:44:16 +0900

    use the v-for for repeated data

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 const products = ['역삼동원룸', '천호동원룸', '마포구원룸'];
+const price = ['50만원', '60만원', '70만원'];
 
 const menus = ['Home', 'Product', 'About'];
 </script>
@@ -9,13 +10,10 @@ const menus = ['Home', 'Product', 'About'];
     <li v-for="(menu, idx) in menus" :key="idx">{{ menu }}</li>
   </ul>
   <img src="/vite.svg" class="logo" alt="Vite logo" />
-  <div></div>
-  {{ products[0] }}
-  <p>50만원</p>
-  {{ products[1] }}
-  <p>50만원</p>
-  {{ products[2] }}
-  <p>50만원</p>
+  <div v-for="(product, index) in products" :key="index">
+    <strong style="display: block">{{ product }}</strong>
+    <p>{{ price[index] }}</p>
+  </div>
 </template>
 
 <style scoped>
