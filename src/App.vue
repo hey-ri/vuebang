fa15a871e2e1756c0c38f572c44da82b1d48372e
hey-ri <kwonhr01@naver.com>
2023-03-06 22:34:47 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+const products = ['역삼동원룸', '천호동원룸', '마포구원룸'];
+
+const menus = ['Home', 'Product', 'About'];
+</script>
+
+<template>
+  <ul>
+    <li v-for="menu in menus" :key="menu">{{ menu }}</li>
+  </ul>
+  <img src="/vite.svg" class="logo" alt="Vite logo" />
+  <div></div>
+  {{ products[0] }}
+  <p>50만원</p>
+  {{ products[1] }}
+  <p>50만원</p>
+  {{ products[2] }}
+  <p>50만원</p>
+</template>
+
+<style scoped>
+ul {
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+}
+li {
+  list-style: none;
+}
+</style>
