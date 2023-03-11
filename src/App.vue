8c192337c2735441e9b40f9e48bf88651fc5672e
hey-ri <kwonhr01@naver.com>
2023-03-11 22:39:32 +0900

    add modal ui

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,30 @@ const onClick = () => {
   <ul>
     <li v-for="(menu, idx) in menus" :key="idx">{{ menu }}</li>
   </ul>
-  <img src="/vite.svg" class="logo" alt="Vite logo" />
+
+  <div class="black-bg">
+    <div class="white-bg">
+      <h3>상세 페이지</h3>
+      <p>내용</p>
+    </div>
+  </div>
+
   <div>
+    <img src="./assets/vue.svg" alt="사진" class="room-img" />
     <strong style="display: block">{{ products[0] }}</strong>
     <p>{{ price[0] }}</p>
     <button @click="count[0]++">허위매물 신고 버튼</button> <span>신고수 : {{ count[0] }}</span>
   </div>
+
   <div>
+    <img src="./assets/vue.svg" alt="사진" class="room-img" />
     <strong style="display: block">{{ products[1] }}</strong>
     <p>{{ price[1] }}</p>
     <button @click="count[1]++">허위매물 신고 버튼</button> <span>신고수 : {{ count[1] }}</span>
   </div>
+
   <div>
+    <img src="./assets/vue.svg" alt="사진" class="room-img" />
     <strong style="display: block">{{ products[2] }}</strong>
     <p>{{ price[2] }}</p>
     <button @click="count[2]++">허위매물 신고 버튼</button> <span>신고수 : {{ count[2] }}</span>
@@ -42,5 +54,31 @@ ul {
 }
 li {
   list-style: none;
+}
+.room-img {
+  width: 100%;
+  margin-top: 50px;
+}
+
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  left: 0;
+  top: 0;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
